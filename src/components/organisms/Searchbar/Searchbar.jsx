b2308ee341e1c609a1154b00ed50acc1ce409e27
mattmalicki <mateusz.malicki96@gmail.com>
2023-10-23 20:38:59 +0200

    Added Searchbar as an organism

---
+++ src/components/organisms/Searchbar/Searchbar.jsx
@@ -0,0 +1,15 @@
+import { ButtonSearch } from 'components/molecules/ButtonSearch/ButtonSearch';
+import { InputSearch } from 'components/atoms/InputSearch/InputSearch';
+import { FormSearch } from '../FormSearch/FormSearch';
+import styles from './Searchbar.module.css';
+
+export const Searchbar = ({ onSubmit }) => {
+  return (
+    <header className={styles.Searchbar}>
+      <FormSearch onSubmit={onSubmit}>
+        <ButtonSearch />
+        <InputSearch />
+      </FormSearch>
+    </header>
+  );
+};
